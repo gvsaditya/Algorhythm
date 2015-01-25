10f081bb89f16919ed4517b9ae2ca189ad7a2c7b
Aditya Gollakota <gvsaditya@gmail.com>
2015-01-25 15:34:31 -0800

    Initial Commit

---
+++ Algorhythm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorhythm
+//
+//  Created by Aditya Gollakota on 1/25/15.
+//  Copyright (c) 2015 Aditya Gollakota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorhythm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorhythm
+//
+//  Created by Aditya Gollakota on 1/25/15.
+//  Copyright (c) 2015 Aditya Gollakota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
